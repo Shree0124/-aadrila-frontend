90593484ba98be5a9e9e948e97f9daa8f2ed97d5
Shreedhar Sanadi <shreedharsanadi99@gmail.com>
2026-01-10 18:54:15 +0530

    new code

--- app/page.jsx
+++ app/page.jsx
@@ -1,44 +1,4 @@
-// import Navbar from "./components/Navbar";
-// import Hero from "./components/Hero";
-// import About from "./components/About";
-// // import Industries from "./components/Industries";
-// // import Products from "./components/Products";
-// // import Blog from "./components/Blog";
-// import Contact from "./components/Contact";
 
-// export default function Home() {
-//   return (
-//     <>
-//       <Navbar />
-
-//       <div className="pt-24"> {/* adjust for fixed navbar */}
-//         <section id="home">
-//           <Hero />
-//         </section>
-
-//         {/* <section id="industries">
-//           <Industries />
-//         </section> */}
-
-//         {/* <section id="products">
-//           <Products />
-//         </section> */}
-// {/* 
-//         <section id="blog">
-//           <Blog />
-//         </section> */}
-
-//         <section id="contact">
-//           <Contact />
-//         </section>
-
-//         <section id="about">
-//           <About />
-//         </section>
-//       </div>
-//     </>
-//   );
-// }
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import About from "./components/About";
